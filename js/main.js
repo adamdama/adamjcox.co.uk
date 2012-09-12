e38e217fdd3f028603c657f3d5e46fd137dd73e3
Adam Cox <adamdama@hotmail.com>
2012-09-12 19:40:23 +0100

    Added canvas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-/* Author:
+/* Author: Adam Cox
 
 */
+
+$(document).ready(function(e)
+{
+	var grid_canvas = document.getElementById('colour_squares_canvas');
+	var grid = grid_canvas.getContext("2d");
+	    
+	grid.fillStyle = "rgb(150,150,150)";
+	grid.fillRect(0,0,100,100); 
+});
