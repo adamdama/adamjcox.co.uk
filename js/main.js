0a51a73e429dcda7b1e011853f2c93d5d66dc0b8
Adam Cox <adamdama@hotmail.com>
2012-09-17 20:39:19 +0100

    Added options var with colours.

--- js/main.js
+++ js/main.js
@@ -219,14 +219,26 @@ CanvasState.prototype.getMouse = function(e)
 // inside the body tag
 //init();
 
+var options = 
+{
+	colours: ['#FF0000',
+				'#FF6600',
+				'#FFFF00',
+				'#33CC00',
+				'#000099'
+	]
+}
+
 var init = function()
 {
 	var s = new CanvasState(document.getElementById('colour_squares_canvas'));
+	
+	
 	s.addShape(new Shape(40, 40, 50, 50));
 	// The default is gray
 	s.addShape(new Shape(60, 140, 40, 60, 'lightskyblue'));
 	// Lets make some partially transparent
 	s.addShape(new Shape(80, 150, 60, 30, 'rgba(127, 255, 212, .5)'));
 	s.addShape(new Shape(125, 80, 30, 80, 'rgba(245, 222, 179, .7)'));
-
+	
 }
