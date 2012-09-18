bb290b88272b82d6072d2cda9579d2379a4bf481
Adam Cox <adamdama@hotmail.com>
2012-09-18 19:20:02 +0100

    Added listen method that may be needed later

--- js/main.js
+++ js/main.js
@@ -254,3 +254,14 @@ var init = function()
 		p.x += m.w;
 	}
 }
+/*
+var listen = function(elem, type, eventHandle) {
+    if (elem == null || elem == undefined) return;
+    if ( elem.addEventListener ) {
+        elem.addEventListener( type, eventHandle, false );
+    } else if ( elem.attachEvent ) {
+        elem.attachEvent( "on" + type, eventHandle );
+    } else {
+        elem["on"+type]=eventHandle;
+    }
+};*/
