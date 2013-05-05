ac55eea833b9e799d5512b4a596c23396c341e6f
Adam Cox <adamdama@hotmail.com>
2013-05-05 20:38:33 +0100

    Updated jQuery. Re added scripts to index.html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-/* Author: Adam Cox
-
-*/
-
-$(document).ready(function(e)
-{
-	var grid_canvas = document.getElementById('colour_squares_canvas');
-	var grid = grid_canvas.getContext("2d");
-	    
-	grid.fillStyle = "rgb(150,150,150)";
-	grid.fillRect(0,0,100,100); 
-});
+/* @author Adam Cox <adamdama@hotmail.com> */
